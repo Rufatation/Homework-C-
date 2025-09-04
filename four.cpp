68b5fe432da95e81058fbfbd2472ec4f3d3459a0
Rufatation <rufat.sadyrbakiyev@gmail.com>
2025-09-04 11:16:11 +0600

    Create four.cpp

---
+++ four.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int d,l;
+    cout<<"Hello! Please input the diameter to find the Circumference."<<endl;
+    cin>>a>>b;
+    l = d*3.14
+    while (d == 0) 
+    {
+        cout<<"A side cant be 0! Input everything again."
+        cin>>a>>b;
+    }
+    cout<<"Circumference:"<<endl;
+    cout<<l<<endl;
+}
