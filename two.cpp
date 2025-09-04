6ee3b738f7aa07871054e859db7687bcfc269610
Rufatation <rufat.sadyrbakiyev@gmail.com>
2025-09-04 11:13:01 +0600

    two.cpp

---
+++ two.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,s;
+    cout<<"Hello! Please input a number to find the Square."<<endl;
+    cin>>a;
+    s = a*a
+    while (a == 0 || b == 0) 
+    {
+        cout<<"A side cant be 0! Input again."
+        cin>>a;
+    }
+    cout<<"Square:"<<endl;
+    cout<<s<<endl;
+}
