50baf1277a2afb7c8e6eec2c0d1b4c870f19ace8
Rufatation <rufat.sadyrbakiyev@gmail.com>
2025-09-04 11:11:47 +0600

    one.cpp

---
+++ one.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,p;
+    cout<<"Hello! Please input a number to find the Perimeter"<<endl;
+    cin>>a;
+    p = a*4
+    while (a == 0) 
+    {
+        cout<<"A side cant be 0! Input the number again."
+        cin>>a;
+    }
+    cout<<"Perimeter:"<<endl;
+    cout<<p<<endl;
+}
